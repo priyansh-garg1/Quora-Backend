29130e2b0b8c8b4dae347f3039b919a0820ce17d
Priyansh Garg <143625974+priyansh-garg1@users.noreply.github.com>
2024-10-27 21:45:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const path = require("path");
 const { v4: uuidv4} = require("uuid");
 const methodOverride = require("method-override");
 
+
 app.use(methodOverride("_method"));
 
 app.use(express.urlencoded({extended: true}));
